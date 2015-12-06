a53a49faeb53851f5a56e01d5f6e30901c17d882
radhasatam <radha.satam@gmail.com>
2015-12-05 20:23:43 -0800

    More Optimized

--- SentimentAnalyze.py
+++ SentimentAnalyze.py
@@ -51,62 +51,67 @@ class GetData:
 		word_features = wordlist.keys()
 		return word_features
 	
+	def run(self):
+		# Regex getting rid of the usernames, periods, commas
+		regex = re.compile('[@]{1}\w*|\.+|\,+|(and)|(the)|(you)|[x][\w|\d]*')
+		fetch = GetData()
+		tok = Tokenizer(preserve_case=False)
+		tweets = []
+		
+		# Filters to select only words that are longer than 2 charcters from both the positive and negative training dataset
+		# Stores in tweets list in the format [(word, sentiment)]
+		for (words, sentiment) in fetch.positive() + fetch.negative():
+			words_filtered = [e.lower().encode('utf-8') for e in tok.tokenize(words) if len(e) >= 3 and not regex.match(e)]
+			tweets.append((words_filtered, sentiment))
+	
+		word_features = fetch.get_word_features(fetch.get_words_in_tweets(tweets))
+	
+		# Function that creates a dictionary that has entires like - {contains(word): False, ...}
+		def extract_features(document):
+			document_words = set(document)
+			features = {}
+			for word in word_features:
+				features['contains(%s)' % word] = (word in document_words)
+			return features
+			
+		# Training set and Naive Bayes Classifier are used to classify the words - ends up classifying the above dictionary format with the sentiment associated
+		training_set = nltk.classify.apply_features(extract_features, tweets)
+		classifier = nltk.NaiveBayesClassifier.train(training_set)
+		
+		raw_test_tweets = fetch.testData()
+		test_tweets = []
+		for words in raw_test_tweets:
+			words_filtered = [e.lower().encode('utf-8') for e in tok.tokenize(words) if len(e) >= 3 and not regex.match(e)]
+			test_tweets.append(words_filtered)
+			
+		poscount = 0
+		negcount = 0
+		sentiment_lables = []
+		for tweet in test_tweets:
+			classifier1 = classifier.classify(extract_features(tweet))
+			sentiment_lables.append(classifier1)
+			if(classifier1 == "positive"):
+				poscount += 1
+			elif(classifier1 == "negative"):
+				negcount += 1
+		
+		# print poscount
+		# print negcount		
+		# print test_tweets 
+		
+		if poscount > negcount:
+			print "The sentiment analysis from the last 600 tweets is - positive"
+		elif negcount > poscount:
+			print "The sentiment analysis from the last 600 tweets is - negative"
+		else:
+			print "The sentiment analysis from the last 600 tweets is - neutral"
+		
+		print "\nOne random tweet and the sentiment -"
+		print raw_test_tweets[2]
+		print sentiment_lables[2]
 
 def main():
-
-	# Regex getting rid of the usernames, periods, commas
-	regex = re.compile('[@]{1}\w*|\.+|\,+')
-	fetch = GetData()
-	tok = Tokenizer(preserve_case=False)
-	tweets = []
+	f = GetData()
+	f.run()
 	
-	# Filters to select only words that are longer than 2 charcters from both the positive and negative training dataset
-	# Stores in tweets list in the format [(word, sentiment)]
-	for (words, sentiment) in fetch.positive() + fetch.negative():
-		words_filtered = [e.lower().encode('utf-8') for e in tok.tokenize(words) if len(e) >= 3 and not regex.match(e)]
-		tweets.append((words_filtered, sentiment))
-
-	word_features = fetch.get_word_features(fetch.get_words_in_tweets(tweets))
-
-	# Function that creates a dictionary that has entires like - {contains(word): False, ...}
-	def extract_features(document):
-		document_words = set(document)
-		features = {}
-		for word in word_features:
-			features['contains(%s)' % word] = (word in document_words)
-		return features
-		
-	# Training set and Naive Bayes Classifier are used to classify the words - ends up classifying the above dictionary format with the sentiment associated
-	training_set = nltk.classify.apply_features(extract_features, tweets)
-	classifier = nltk.NaiveBayesClassifier.train(training_set)
-	
-	raw_test_tweets = fetch.testData()
-	test_tweets = []
-	for words in raw_test_tweets:
-		words_filtered = [e.lower().encode('utf-8') for e in tok.tokenize(words) if len(e) >= 3 and not regex.match(e)]
-		test_tweets.append(words_filtered)
-	
-	poscount = 0
-	negcount = 0
-	sentiment_lables = []
-	for tweet in raw_test_tweets:
-		classifier1 = classifier.classify(extract_features(tweet))
-		sentiment_lables.append(classifier1)
-		if(classifier1 == "positive"):
-			poscount += 1
-		elif(classifier1 == "negative"):
-			negcount += 1
-			
-	if poscount > negcount:
-		print "The sentiment analysis from the last 10 tweets is - positive"
-	elif negcount > poscount:
-		print "The sentiment analysis from the last 10 tweets is - negative"
-	else:
-		print "The sentiment analysis from the last 10 tweets is - neutral"
-	
-	print "\nOne random tweet and the sentiment -"
-	print raw_test_tweets[1]
-	print sentiment_lables[1]
-
-main()	
-	
+main()
