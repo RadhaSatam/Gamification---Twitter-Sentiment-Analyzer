1195571b66521428d596943536ea7b7512c2e804
radhasatam <radha.satam@gmail.com>
2015-12-05 23:34:09 -0800

    Progress with the interface

--- pygameDemo.py
+++ pygameDemo.py
@@ -1,8 +1,11 @@
-import pygame
+import pygame, math
 from SentimentAnalyze import GetData
+from GetTweets import GetTweets
+
+sentimentAnalysis = GetData()
+getTweets = GetTweets()
 
 pygame.init()
-pygame.font.init()
 
 # Defining colors 
 white = (255,255,255)
@@ -14,19 +17,29 @@ blue = (0,0,255)
 display_width = 800
 display_height = 600
 
+half_width = display_width/2
+half_height = display_height/2
+
+x = half_width -160
+y = half_height - 15
+
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Twitter Sentiment Analyzer')
 
 gameExit = False
 
-clock = pygame.time.Clock()
+# clock = pygame.time.Clock()
 pygame.font.init()
-font = pygame.font.Font(None, 50)
 
 # Function to display text of specified color to user
-def message_to_screen(msg, color):
+def message_to_screen(msg, color, location, fontsize):
+	font = pygame.font.Font(None, fontsize)
 	screen_text = font.render(msg, True, color)
-	gameDisplay.blit(screen_text, [(display_width/2)-250, display_height/2-20])
+	gameDisplay.blit(screen_text, location )
+
+
+screen = 0
+query = ""
 
 # Main Game Loop 
 while not gameExit:
@@ -34,12 +47,34 @@ while not gameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
+		if screen == 0:
+			if(event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN):
+				screen = 1
+		if screen == 1:
+			if event.type == pygame.KEYDOWN:
+				if event.unicode.isalpha():
+					query += event.unicode
+				elif event.key == pygame.K_BACKSPACE:
+					query = query[:-1]
+				elif event.key == pygame.K_SPACE:
+					query += " "
+				elif event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+					if(query!=""):
+						screen = 2
+						getTweets.getting_query_result(query)
 			
 	gameDisplay.fill(white)
-	message_to_screen("Twitter Sentiment Analyzer", black)
-	pygame.display.update()
-
+	if(screen == 0):
+		message_to_screen("Twitter Sentiment Analyzer", black, [half_width-250, half_height-80], 50)
+		message_to_screen("Press any key to continue...", green, [half_width-250, half_height-20], 30)
+	elif(screen == 1):
+		message_to_screen("Enter a query search term - ", black, [half_width-250, half_height-80], 50)
+		pygame.draw.circle(gameDisplay, (192,192,192), (half_width,half_height + 150), 50)
+		message_to_screen(query, black, [half_width-250, half_height], 30)
+	elif(screen== 2):
+		message_to_screen("Your query:", black, [100, 50], 30)
+		message_to_screen(query, blue, [100, 100], 30)
 		
-
+	pygame.display.update()
 pygame.quit()
 quit()
